53905f10fd49bb1cbc0993b723fd6986570b0514
Tedros Nigus Asefa <teddynigus2@gmail.com>
2026-05-16 06:49:06 +0100

    Update application name in configuration

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Unifiedtransform'),
+    'name' => env('APP_NAME', 'MMM Boarding High School Managing System'),
 
     /*
     |--------------------------------------------------------------------------
